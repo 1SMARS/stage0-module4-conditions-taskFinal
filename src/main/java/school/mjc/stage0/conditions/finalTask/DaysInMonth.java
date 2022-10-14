d6df35f294f3cda9197b85420d2b40f876510df4
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-15 01:28:43 +0600

    Update DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,18 +2,26 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        
-        boolean tru = year % 4 == 0 && year % 100 == 0 && year % 400 == 0;
+       
 
         switch (month) {
             case 1:
                 System.out.println(31);
                 break;
             case 2:
-                if (tru)
-                    System.out.println(29);
-                else
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
+                            System.out.println(29);
+                        } else {
+                            System.out.println(28);
+                        }
+                    } else {
+                        System.out.println(29);
+                    }
+                } else {
                     System.out.println(28);
+                }
                 break;
             case 3:
                 System.out.println(31);
