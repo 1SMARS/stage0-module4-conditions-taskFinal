c04fcfed4ac1fd9e4d4bce9b1458576f327097b2
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-15 01:04:22 +0600

    Update BitwiseValuesSwap.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println(x + " " + y);
     }
 }
